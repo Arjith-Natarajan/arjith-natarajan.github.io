5dcd7253827da1bd72883eadb06de9a38bc24f67
Arjith-Natarajan <arjith496@gmail.com>
2017-04-18 01:01:28 +0530

    Completed Basic Routing Features using angular

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,12 @@ app.config(['$locationProvider', function($locationProvider) {
 }]);
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
+  .when("/",{templateUrl :"../../index.html", controller: "PageCtrl"})
+  .when("/services",{templateUrl :"../../templates/services.html", controller: "PageCtrl"})
+  .when("/portfolio",{templateUrl :"../../templates/portfolio-item.html", controller: "PageCtrl"})
   .when("/about",{templateUrl :"../../templates/about.html", controller: "PageCtrl"})
+  .when("/team",{templateUrl :"../../templates/portfolio-1-col.html", controller: "PageCtrl"})
+  .when("/contact",{templateUrl :"../../templates/contact.html", controller: "PageCtrl"})
 }]);
 
 app.controller('PageCtrl', function (/* $scope, $location, $http */) {
