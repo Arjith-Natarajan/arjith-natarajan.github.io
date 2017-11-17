46fd2f564dd36fdc4e98f712aedfbb20c5b16ad7
Arjith Natarajan <arjith@sigaramtech.com>
2017-11-18 04:23:53 +0530

    Changed into a react seed project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+
+  // Properties are way of passing data into components
+  // can think of them like custom attritbutes that can be set values
+  // ----------------------------------------
+  <App txt="My-React-App"/>,
+  document.getElementById('root')
+);
